2e527bde425c08c8272fe5741fd1139a11fb4a11
gyf <charon0707@163.com>
2023-11-27 22:16:12 +0800

    Feature: test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,13 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import unittest
 from src.merge_sort import merge_sort
 
-def test_case1():
-    # 测试一个正确的用例
-    assert merge_sort([4, 3, 2, 1, 6]) == [1, 2, 3, 4, 6]
+class TestMergeSort(unittest.TestCase):
+    def test_case1(self):
+        # 测试一个正确的用例
+        assert merge_sort([4, 3, 2, 1, 6]) == [1, 2, 3, 4, 6]
 
+if __name__ == '__main__':
+    unittest.main()
